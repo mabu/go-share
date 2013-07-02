d6815158ab67cb837cf920f69fc7793f879172e0
Martynas Budriūnas <martynas.budriunas@gmail.com>
2013-07-02 17:39:49 +0300

    Use log package.

--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
 	"github.com/mabu/go-share/share"
 	"github.com/mewpkg/gopass"
 	"os"
 	"os/signal"
 )
-
-func printAndExit(msgs ...interface{}) {
-	fmt.Println(msgs...)
-	os.Exit(1)
-}
 
 func main() {
 	port := flag.Int("p", 80, "port number")
@@ -21,28 +16,27 @@ func main() {
 
 	password, err := gopass.GetPass("Please enter a password for file upload: ")
 	if err != nil {
-		printAndExit("Error:", err)
+		log.Fatalln("Error:", err)
 	}
 	if p, err := gopass.GetPass("Please repeat the password: "); err != nil {
-		printAndExit("Error:", err)
+		log.Fatalln("Error:", err)
 	} else if password != p {
-		printAndExit("Passwords do not match.")
+		log.Fatalln("Passwords do not match.")
 	}
 
 	c := make(chan os.Signal)
 	signal.Notify(c)
 	go func() {
 		sig := <-c
-		fmt.Printf("Caught signal %v, exiting...", sig)
-		os.Exit(0)
+		log.Fatalf("Caught signal %v, exiting...\n", sig)
 	}()
 
 	s, err := share.New(*directory, password)
 	if err != nil {
-		printAndExit("Could not create server:", err)
+		log.Fatalln("Could not create server:", err)
 	}
-	fmt.Printf("Starting go-share on port %d.\n", *port)
+	log.Printf("Starting go-share on port %d.\n", *port)
 	if err := s.Start(*port); err != nil {
-		printAndExit("Could not start server:", err)
+		log.Fatalln("Could not start server:", err)
 	}
 }
